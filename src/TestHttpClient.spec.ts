a8819bdc3f29b8467fdd6cb674e2537f2270572d
harunou <d.harunou@gmail.com>
2023-05-15 20:56:08 +0200

    TestHttpClient: add several tests

--- src/TestHttpClient.spec.ts
+++ src/TestHttpClient.spec.ts
@@ -14,12 +14,13 @@ describe(`${TestHttpClient.name}`, () => {
             expect(instance0).toEqual(instance1);
         });
     });
-    describe('singular pending request operations', () => {
+    describe('singular pending request', () => {
         it('finds pending request by url', () => {
             void httpClient.request(new Request(endpoint));
             const pendingRequest = httpClient.expectOne(endpoint);
             expect(pendingRequest.request.url).toEqual(endpoint);
         });
+
         it('finds pending request by url and init', () => {
             void httpClient.request(new Request(endpoint, { method: 'POST' }));
             const pendingRequest = httpClient.expectOne(endpoint, { method: 'POST' });
@@ -29,33 +30,45 @@ describe(`${TestHttpClient.name}`, () => {
         it('throws an error if a pending request in not found', () => {
             expect(() => httpClient.expectOne(endpoint)).toThrow();
         });
+
         it('does not remove pending request from the client with expectOne', () => {
             void httpClient.request(new Request(endpoint, { method: 'POST' }));
             const pendingRequest0 = httpClient.expectOne(endpoint, { method: 'POST' });
             const pendingRequest1 = httpClient.expectOne(endpoint, { method: 'POST' });
             expect(pendingRequest0).toEqual(pendingRequest1);
         });
+
+        it('allows to remove existing pending request', () => {
+            void httpClient.request(new Request(endpoint));
+            httpClient.removeOne(endpoint);
+            expect(() => httpClient.expectOne(endpoint)).toThrow();
+        });
+
         it('throws an error if tries to remove non existing pending request', () => {
             expect(() => httpClient.removeOne(endpoint)).toThrow();
         });
+
         it('allows to resolve pending request', async () => {
             const resolveValue = 3;
             const promise = httpClient.request<number>(new Request(endpoint));
             httpClient.expectOne<number>(endpoint).resolve(resolveValue);
             await expect(promise).resolves.toEqual(resolveValue);
         });
+
         it('removes pending request from http client once resolved', () => {
             const resolveValue = 3;
             void httpClient.request<number>(new Request(endpoint));
             httpClient.expectOne<number>(endpoint).resolve(resolveValue);
             expect(() => httpClient.expectOne<number>(endpoint)).toThrow();
         });
+
         it('allows to reject pending request', async () => {
             const error = new Error('error');
             const promise = httpClient.request(new Request(endpoint));
             httpClient.expectOne(endpoint).reject(error);
             await expect(promise).rejects.toEqual(error);
         });
+
         it('removes pending request from http client once rejected', () => {
             const error = new Error('error');
             void httpClient.request<number>(new Request(endpoint)).catch(() => {
@@ -66,23 +79,34 @@ describe(`${TestHttpClient.name}`, () => {
             expect(true).toEqual(true);
         });
     });
-    describe('multiple pending request operations', () => {
+    describe('multiple pending requests', () => {
         it('allows to find all pending request by url and init', () => {
             void httpClient.request(new Request(endpoint, { method: 'POST' }));
             void httpClient.request(new Request(endpoint, { method: 'POST' }));
             const pendingRequests = httpClient.expect(endpoint, { method: 'POST' });
             expect(pendingRequests.length).toEqual(2);
         });
+
+        it('throws an error if pending requests ar not found', () => {
+            expect(() => httpClient.expect(endpoint, { method: 'POST' })).toThrow();
+        });
+
         it('allows to delete all pending requests by url and init', () => {
             void httpClient.request(new Request(endpoint, { method: 'POST' }));
             void httpClient.request(new Request(endpoint, { method: 'POST' }));
             httpClient.remove(endpoint, { method: 'POST' });
             expect(() => httpClient.expect(endpoint, { method: 'POST' })).toThrow();
         });
+
+        it('throws an error if tries to remove non existing pending requests', () => {
+            expect(() => httpClient.remove(endpoint, { method: 'POST' })).toThrow();
+        });
+
         it('verifies unresolved requests', () => {
             void httpClient.request(new Request(endpoint));
             expect(() => httpClient.verify()).toThrow();
         });
+
         it('cleans all pending requests', () => {
             void httpClient.request(new Request(endpoint));
             httpClient.clean();
